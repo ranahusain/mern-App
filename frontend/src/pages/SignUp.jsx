56703423ee557986742059d13878658f87312c90
ranahusain <capthussain2point0@gmail.com>
2025-06-27 17:10:11 +0500

    change done

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -12,11 +12,6 @@ const SignUp = () => {
           <div className="form-inputs">
             <div className="heading">
               <h1>Sign Up</h1>
-              <p>
-                Join our community and start your journey with us today. Sign up
-                to access exclusive features, connect with others, and stay
-                up-to-date with the latest updates.
-              </p>
             </div>
             <h4>Name</h4>
             <input
