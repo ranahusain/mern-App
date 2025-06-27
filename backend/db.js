adc3ba813da4480cbd44f2c03ca88fe5c7a119df
ranahusain <capthussain2point0@gmail.com>
2025-06-27 11:36:38 +0500

    MONGODB_URI add

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb://localhost:27017/test`, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
     });
     console.log(`MongoDB Connected`);
