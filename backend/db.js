1cae52f750b6eaa56b17009f07afef0e30f9e7f0
Hussain Ashraf <91388359+ranahusain@users.noreply.github.com>
2025-06-27 13:13:11 +0500

    Revert "Routes"

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      // useNewUrlParser: true,
+    const conn = await mongoose.connect(`mongodb://localhost:27017/test`, {
+      useNewUrlParser: true,
     });
     console.log(`MongoDB Connected`);
   } catch (error) {
--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,10 @@
 const express = require("express");
 const connectDB = require("./db");
-const users = require("./routes/users");
-const cors = require("cors");
-
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 connectDB();
-
-// app.use("*", (req, res) => {
-//   res.status(404).json({ message: "Route not found" });
-// });
-
-// app.use(cors());
-
-app.use("/api", users);
 
 app.get("/", (req, res) => {
   console.log("I am inside home page router");
