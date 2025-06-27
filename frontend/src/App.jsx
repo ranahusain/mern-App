f4360aa5970172781880f417b7391711c55bad6f
ranahusain <capthussain2point0@gmail.com>
2025-06-27 16:59:15 +0500

    pages add login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <>{/* <h1 class="text-3xl font-bold underline">Hello world!</h1> */}</>
+    <>
+      <Login />
+    </>
   );
 }
 
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,53 @@
+import "../index.css";
+import form from "../assets/images/form.png";
+const SignUp = () => {
+  return (
+    <>
+      <div className="main-div">
+        <div className="form">
+          <div className="form img">
+            <img src={form} alt="" />
+          </div>
+
+          <div className="form-inputs">
+            <div className="heading">
+              <h1>Sign Up</h1>
+              <p>
+                Join our community and start your journey with us today. Sign up
+                to access exclusive features, connect with others, and stay
+                up-to-date with the latest updates.
+              </p>
+            </div>
+            <h4>Name</h4>
+            <input
+              className="inputs"
+              type="text"
+              placeholder="enter your mail"
+              id="inputEmail"
+            />
+            <h4>E-mail</h4>
+            <input
+              className="inputs"
+              type="text"
+              placeholder="enter your mail"
+              id="inputEmail"
+            />
+            <h4>Password</h4>
+            <input
+              className="inputs"
+              type="password"
+              name=""
+              id="inputPassword"
+              placeholder="enter your password"
+            />
+            <button className="btn" onclick="window.open('./login.html')">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
