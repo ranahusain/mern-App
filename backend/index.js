b8b4f24f27753acd1b8f0a04bec8af15fff7b9d5
ranahusain <capthussain2point0@gmail.com>
2025-06-30 11:37:43 +0500

    conflic in main solve

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const connectDB = require("./db");
-<<<<<<< HEAD
-=======
 const users = require("./routes/users");
 const login = require("./routes/login");
 const signup = require("./routes/signup");
 
 const cors = require("cors");
 
->>>>>>> origin/frontend
 const app = express();
 const PORT = 3000;
 
@@ -16,15 +13,12 @@ app.use(express.json());
 
 connectDB();
 
-<<<<<<< HEAD
-=======
 app.use(cors());
 
 app.use("/api", users);
 app.use("/api", login);
 app.use("/api", signup);
 
->>>>>>> origin/frontend
 app.get("/", (req, res) => {
   console.log("I am inside home page router");
   res.send("Welcome");
